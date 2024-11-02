43b4c16238207feb0cd75cf212f9700b39abdd68
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-03 00:10:02 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,12 +3,12 @@ import socket  # noqa: F401
 
 def main():
     # Uncomment this to pass the first stage
-    while True:
         server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-        client, address = server_socket.accept() # wait for client
-
-        client.send(b"+PONG\r\n")
-        client.close()
+        client, address = server_socket.accept()  # wait for client
+        while True:
+            command = client.recv(1024).decode()
+            if 'ping' in command.lower():
+                client.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
