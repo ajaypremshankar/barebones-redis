1189a1255a53405a305919dbd0de11e7cd106d66
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-03 09:04:40 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,48 @@
 import socket  # noqa: F401
 import threading
+from dataclasses import dataclass
+
+
+@dataclass
+class FiredCommand:
+    command: str
+    params: [str] = None
+
+
+def encode_resp(raw_resp: str):
+    return f"+{raw_resp}\r\n".encode()
+
+
+def parse_command(raw_command: str):
+    raw_command_arr = raw_command.split("\r\n")
+    command_and_param_length = int(raw_command_arr[0].replace("*", "")) if raw_command_arr[0].startswith("*") else 0
+    if command_and_param_length == 0 or len(raw_command_arr) < 4:
+        raise Exception("Invalid command")
+
+    command = raw_command_arr[2]
+    params_length = command_and_param_length - 1
+
+    params = []
+    if params_length > 0:
+        for index in range(params_length):
+            params.append(raw_command_arr[index * 2 + 4])  # first param start at index 4, if available.
+
+    return FiredCommand(command, params)
+
 
 def spawn_requests(client: socket.socket):
     connected = True
     while connected:
         command = client.recv(1024).decode()
+        fired_command = parse_command(command)
+
+        match fired_command.command.lower():
+            case 'ping':
+                client.send(encode_resp("PONG"))
+            case 'echo':
+                client.send(encode_resp(fired_command.params[0]))
+
         connected = bool(command)
-        if 'ping' in command.lower():
-            client.send(b"+PONG\r\n")
 
 
 def main():
@@ -16,6 +51,7 @@ def main():
         client, address = server_socket.accept()  # wait for client
         t = threading.Thread(target=lambda: spawn_requests(client))
         t.start()
+
 
 if __name__ == "__main__":
     main()
