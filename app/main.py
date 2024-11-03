312016744c024142bd8a1a95f08b0512cef0b7a5
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-11-03 08:36:51 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
 import socket  # noqa: F401
+import threading
+
+def spawn_requests(client: socket.socket):
+    connected = True
+    while connected:
+        command = client.recv(1024).decode()
+        connected = bool(command)
+        if 'ping' in command.lower():
+            client.send(b"+PONG\r\n")
 
 
 def main():
-    # Uncomment this to pass the first stage
-        server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    while True:
         client, address = server_socket.accept()  # wait for client
-        while True:
-            command = client.recv(1024).decode()
-            if 'ping' in command.lower():
-                client.send(b"+PONG\r\n")
+        t = threading.Thread(target=lambda: spawn_requests(client))
+        t.start()
 
 if __name__ == "__main__":
     main()
